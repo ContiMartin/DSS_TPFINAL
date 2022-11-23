5ca8b2f210378d94f4cc6ffa6111246afe361c30
Jony <jntorres2014@gmail.com>
2022-11-23 19:05:08 -0300

    subiendo tp

--- hello.py
+++ hello.py
@@ -14,6 +14,8 @@ def inicio():
 
 @app.route('/procesar', methods=['POST'])
 def procesar():
+    print("datos")
+    print(request.form)
     # cantidadEquipos = int(request.form.get("cantidadEquipos"))
     # nombreTorneo = request.form.get("nombreTorneo")
     # listas = request.form.getlist("lista[]")
@@ -22,12 +24,26 @@ def procesar():
     # datosSimulacion,datos=simu.simular(listas,cantidadEquipos,1)
     # simu.obtenerGrafico(datos)
     return render_template("index.html") #, datos = datosSimulacion)
-
+@app.route('/funcion', methods=['POST'])
+def funcion():
+    print("hola mundo")
 # @app.route('/graficar')
 # def graficar():
 #     return render_template('graficar.html', rutas=list(rutas.values()))
 
 
+@app.route('/my-api', methods = ['GET'])
+def hello():
+    name = request.args.get('name')
+
+    if name is None:
+        text = 'Hello!'
+
+    else:
+        text = 'Hello ' + name + '!'
+
+    return jsonify({"message": text})
+
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5000)
